51fd1ca769965caee41cc80b46c2f3316fd41274
Yuki <Yuki.Kawata@jrits.ricoh.co.jp>
2015-06-25 15:21:03 +0900

    既存のJanBOTからルーチン周りをコピー

---
+++ src/main/java/wiz/project/janbot/game/CallType.java
@@ -0,0 +1,48 @@
+/**
+ * CallType.java
+ * 
+ * @Author
+ *   D-freak
+ */
+
+package wiz.project.janbot.game;
+
+
+
+/**
+ * 鳴きタイプ
+ */
+public enum CallType {
+    
+    /**
+     * ロン
+     */
+    RON,
+    
+    /**
+     * チー
+     */
+    CHI,
+    
+    /**
+     * ポン
+     */
+    PON,
+    
+    /**
+     * 大明カン
+     */
+    KAN_LIGHT,
+    
+    /**
+     * 加カン
+     */
+    KAN_ADD,
+    
+    /**
+     * 暗カン
+     */
+    KAN_DARK,
+    
+}
+
